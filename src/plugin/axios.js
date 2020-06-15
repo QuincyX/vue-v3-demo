c02cad556d430298e4ca00c238a95a80aad83b9c
Quincy <likequincy@outlook.com>
2020-06-15 13:55:53 +0800

    2

---
+++ src/plugin/axios.js
@@ -0,0 +1,6 @@
+/*
+ * @Date: 2020-06-06 15:58:29
+ * @LastEditors: Quincy
+ * @LastEditTime: 2020-06-06 15:58:29
+ * @Description:
+ */
---
+++ src/views/index.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+.page
+  p this is index page
+  p {{msg}}
+  p {{state.count}}
+  p {{state.double}}
+  .btn(@click="increment") button
+</template>
+
+<script>
+import { reactive, computed } from 'vue'
+import consola from 'consola'
+
+export default {
+  data() {
+    return {
+      msg: 'ok'
+    }
+  },
+  setup() {
+    consola.info("info")
+    consola.success("success")
+    consola.error("error")
+    const state = reactive({
+      count: 0,
+      double: computed(() => state.count * 2)
+    })
+    function increment() {
+      state.count++
+    }
+
+    return {
+      state,
+      increment
+    }
+  }
+}
+</script>
